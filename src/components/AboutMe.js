3448e36c69e217427b450036fd10eaf00b0d36ce
Brian Smith <bjtsmith23@gmail.com>
2022-01-19 18:00:37 -0500

    added Header, Navbar and Aboutme with preliminary styling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,22 +1,22 @@
 import React from "react";
-import portrait from '../images/picofme.jpg'; 
+import portrait from "../images/picofme.jpg";
 
 function AboutMe() {
   return (
-    <div class="aboutme">
+    <div>
       <h2 id="AboutMe">About Me</h2>
-        <img
-          class="pic"
-          src={portrait}
-          alt="A picture of me smiling"
-        />
-      <p>
-        I am from Little Silver New Jersey and I am looking to become a master
-        of coding. I graduated Manhattan College in 2015 with a Finance Degree
-        but I have now just realized I want to get into coding. I know it wont
-        be easy but with my work ethic and perserverence, I am confident I will
-        succeed!!
-      </p>
+      <div class="aboutContents">
+        <img class="portrait" src={portrait} alt="Me smiling" />
+
+        <p>
+          I am from Little Silver New Jersey and I am looking to become a master
+          of coding. I graduated Manhattan College in 2015 with a Finance Degree
+          but I have now just realized I want to get into coding. I know it wont
+          be easy but with my work ethic and perserverence, I am confident I
+          will succeed!!
+        </p>
+      </div>
+
       <h2 class="work">My Works</h2>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
     
-    <header className="header">
+    <header class="header">
     <h1>Brian Smith</h1>
     </header>
     
