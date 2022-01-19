1b555b4d87f9709f7133d1defa26d0e88a8bc7f4
Brian Smith <bjtsmith23@gmail.com>
2022-01-19 18:21:44 -0500

    adding pics

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
 import AboutMe from './components/AboutMe';
+import Works from './components/Works';
 
 function App() {
   return (
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import ''
+import codeQuiz from "../images/CodeQuiz.png";
+import refactor from "../images/Refactor.png";
+import workday from "../images/WorkDayScheduler";
 
 function Works() {
+
+    <div class="Works" id="MyWorks">
+    <div class="hw" id="Code">
+      <a href="https://bjtsmith23.github.io/Code-Quiz/"/>
+        <img src={codeQuiz} alt="Code Quiz Project"/>
+      <h3>Code Quiz</h3>
+    </div>
+    <div class="hw" id="Refactor">
+      <a href="https://bjtsmith23.github.io/Refactor/">
+        <img src={refactor} alt="Refactor Project"/>
+      </a>
+      <h3>My Refactored Website</h3>
+    </div>
+    <div class="hw" id="Workday">
+      <a href="https://bjtsmith23.github.io/Work-Day-Scheduler/">
+        <img src={workday} alt="Work Day Scheduler Project"/>
+      </a>
+      <h3>Work Day Scheduler</h3>
+    </div>
+    <div class="hw" id="Code">
+      <a href="https://natpitt2393.github.io/Everyone-s-A-Critic/">
+        <img src="assets/images/critic.png"/>
+      </a>
+      <h3>Project 1</h3>
+    </div>
+</div>
+
 
 }
