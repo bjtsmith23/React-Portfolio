ae372dca170ad67cdc7e21966b74c7012157de27
Brian Smith <bjtsmith23@gmail.com>
2022-01-20 21:48:24 -0500

    fixed resume link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
     return (
     
         <nav>
-        <button type="button" class="btn btn-primary btn-sm"><a href="../Resume.pdf" target="_blank">Resume</a></button>
+        <button type="button" class="btn btn-primary btn-sm"><a href="https://drive.google.com/file/d/19nFbGKVYRzbx9YjQ73CYdwyzapxogIH9/view?usp=sharing" target="_blank">Resume</a></button>
         <button type="button" class="btn btn-primary btn-sm"><a href="#MyWorks">My Works</a></button>
         <button type="button" class="btn btn-primary btn-sm"><a href="#contact">Contact</a></button>
         </nav>
