6120d1970f44cc0f5cafb5933d393aec09d4dd80
Brian Smith <bjtsmith23@gmail.com>
2022-01-25 23:28:55 -0500

    fixed up app js and render pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
-import NavBar from './components/Navigation';
+import Navigation from './components/Navigation';
 import AboutMe from './components/pages/AboutMe';
-import Works from './components/pages/Portfolio';
+import Portfolio from './components/pages/Portfolio';
 import Footer from './components/Footer';
+import Contact from './components/pages/Contact';
 
 function App() {
+  // setting the default page to AboutMe
+  const [currentPage, setCurrentPage] = useState("AboutMe");
+
+  const renderPage = () => {
+    if (currentPage === "AboutMe") {
+      return <AboutMe/>;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio/>;
+    }
+    if (currentPage === "Resume") {
+      return <Resume/>;
+    }
+    return <Contact/>;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <>
-    <Header/>
-    <Navigation/>
-    <AboutMe/>
-    <Portfolio/>
-    <Footer/>
     
-    </>
-    
-    
-    
-  );
-}
 
 export default App;
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -40,4 +40,4 @@ function Portfolio() {
   );
 }
 
-export default Works;
+export default Portfolio;
